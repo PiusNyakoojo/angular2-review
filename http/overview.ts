8ba01afacbfa7141617370cfddccb9958898fec9
PiusNyakoojo <pnyakoojo.16@gmail.com>
2016-07-12 20:43:20 -0500

    Added info

--- http/overview.ts
+++ http/overview.ts
@@ -2,7 +2,7 @@
 	The observable pattern is certain pattern where we have a stream of data (or event which emits data). These are observables,
 	something.. something that we can observe to see what happens.
 
-	On the other end, we have a Subscriber which listens to the events.
+	On the other end, we have a Subscriber (or observer) which listens to the events.
 
 	By default, angular 2 uses the Observable pattern to access the web. So you can think of the response that we get from
 	a server as a data stream that we listen to once we've sent a request. We know we'll probably get a response eventually,
@@ -14,4 +14,30 @@
 
 	We can also cancel observables so we stop listening to responses. So Observables aren't exactly callbacks.. but
 	you can think of them as having similar functionality.
+
+	If you would prefer to use Promises, you can call the toPromise() method on observables
+	
 */
+
+//--------------------------- index.html ------------------------
+/*
+<script src="node_modules/angular2/bundles/http.js"></script>
+
+*/
+
+
+//------------------------ boot.ts or main.ts --------------------
+/*
+import {HTTP_PROVIDERS} from "@angular/core";
+
+bootstrap(AppComponent, [HTTP_PROVIDERS]);
+*/
+
+//--------------------------- data.service.ts ------------------------------------
+import {Injectable} from "@angular/core";
+import {Http} from "@angular/core";
+
+@Injectable()
+export class DataService {
+	constructor(private _http: Http) {}
+}
---
+++ typescript/overview.ts
@@ -0,0 +1,25 @@
+/*
+	Typescript is a superset of Javascript.
+		- Everything you can write in javascript is valid in Typescript.
+		- Typescript provides extra functionality
+			- Types
+			- Modules
+			- Classes
+
+	Why use Typescript over Javascript?
+		- General Reasons
+			- Strong typings: allows compile-time Errors, IDE support (autocomplete, eroors, ...)
+			- Use next generation JS features today: Classes, Imports / Exports, ...
+			- We also get features that JS doesn't have: Interfaces, Classes, ...
+		- Angular 2 Reasons
+			- Documentation & Support: Typescript has far more documentation and examples when it comes to Angular 2 (more than dart or javascript)
+			- Main language: Angular 2 chose TypeScript as main Language
+
+
+	To install Typescript:
+		npm install typescript -g
+
+
+
+
+*/
